2dcc00389ecccfbddfc2b20068d66f81fd60292c
Zycky <valdebenito.m.a@gmail.com>
2024-07-30 15:26:11 -0400

    Estructura colaboradores Terminada

--- lib/routes/colaboradores.dart
+++ lib/routes/colaboradores.dart
@@ -1,138 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class ImageData {
-  final String image;
-  final String title;
-  final String description;
 
-  ImageData({
-    required this.image,
-    required this.title,
-    required this.description,
+// Modelo de datos para un colaborador
+class Colaborador {
+  final String nombre;
+  final String descripcion;
+  final String imagenPath;
+  final List<String> enlaces; // Nuevo parámetro
+
+  Colaborador({
+    required this.nombre,
+    required this.descripcion,
+    required this.imagenPath,
+    required this.enlaces, // Inicialización del nuevo parámetro
   });
 }
 
-
-
+// Página principal que muestra la lista de colaboradores
 class ColaboradoresPage extends StatelessWidget {
-  const ColaboradoresPage({super.key});
+  // Lista de colaboradores
+  final List<Colaborador> colaboradores = [
+    Colaborador(
+      nombre: 'Pablo Mansilla Quiñones',
+      descripcion: 'Jefe del Proyecto',
+      imagenPath: 'assets/img/pablo.jpg',
+      enlaces: ['https://www.researchgate.net/profile/Pablo-Mansilla-Quinones'],
+    ),
+    Colaborador(
+      nombre: 'Colaborador 2',
+      descripcion: 'Descripción del colaborador 2',
+      imagenPath: 'assets/images/image2.png',
+      enlaces: ['https://example.com/3'],
+    ),
+    // Agrega más colaboradores aquí
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Colaboradores")),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 500),
-          child: ListView.builder(
-            itemCount: _images.length,
-            itemBuilder: (BuildContext context, int index) {
-              final imageData = _images[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => SecondPage(heroTag: index)));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Hero(
-                        tag: index,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            imageData.image,
-                            width: 200,
-                            fit: BoxFit.cover, // Ajusta la imagen para que se ajuste correctamente al contenedor
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              imageData.title,
-                              style: Theme.of(context).textTheme.headlineLarge, // Ajusta el estilo del título
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              imageData.description,
-                              style: Theme.of(context).textTheme.bodyMedium, // Ajusta el estilo de la descripción
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Colaboradores'),
       ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  final int heroTag;
-
-  const SecondPage({super.key, required this.heroTag});
-
-  @override
-  Widget build(BuildContext context) {
-    final imageData = _images[heroTag];
-    return Scaffold(
-      appBar: AppBar(title: const Text("Colaboradores")),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Hero(
-                tag: heroTag,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.network(
-                    imageData.image,
-                    fit: BoxFit.cover, // Ajusta la imagen para que se ajuste correctamente al contenedor
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  imageData.title,
-                  style: Theme.of(context).textTheme.headlineLarge, // Ajusta el estilo del título en la página de detalles
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  imageData.description,
-                  style: Theme.of(context).textTheme.bodyMedium, // Ajusta el estilo de la descripción en la página de detalles
+      body: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        itemCount: colaboradores.length,
+        itemBuilder: (context, index) {
+          final colaborador = colaboradores[index];
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0),
+            padding: EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
                 ),
               ],
             ),
-          ),
-        ],
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(colaborador.imagenPath),
+                radius: 30.0,
+              ),
+              title: Text(
+                colaborador.nombre,
+                style: TextStyle(color: Colors.black, fontSize: 18.0),
+              ),
+              subtitle: Text(
+                colaborador.descripcion,
+                style: TextStyle(color: Colors.black54, fontSize: 14.0),
+              ),
+              onTap: () {
+                // Mostrar el diálogo con los detalles del colaborador
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(colaborador.nombre),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage(colaborador.imagenPath),
+                              radius: 50.0,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            colaborador.nombre,
+                            style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Colors.black),
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            colaborador.descripcion,
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.black87),
+                          ),
+                          SizedBox(height: 16.0),
+                          // Mostrar enlaces si existen
+                          if (colaborador.enlaces.isNotEmpty) ...[
+                            Text(
+                              'Enlaces:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: colaborador.enlaces.map((enlace) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    // Abrir el enlace en el navegador
+                                    _launchURL(enlace);
+                                  },
+                                  child: Text(
+                                    enlace,
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      decoration: TextDecoration.underline,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(); // Cierra el diálogo
+                          },
+                          child: Text('Cerrar'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
+
+  // Función para abrir enlaces en el navegador
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'No se pudo abrir el enlace $url';
+    }
+  }
 }
 
-final List<ImageData> _images = [
-  ImageData( //
-    image: 'assets/img/pablo.jpg',
-    title: 'Pablo Mansilla',
-    description: '...',
-  ),
-   ///Agregar mas colaboradores aca
-
- 
-];
