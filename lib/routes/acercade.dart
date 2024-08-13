048908b076df2582bf5dedbed8a380074671da96
Zycky <valdebenito.m.a@gmail.com>
2024-08-13 10:20:39 -0400

    Se arregla funcionalidad

--- lib/routes/acercade.dart
+++ lib/routes/acercade.dart
@@ -16,38 +16,17 @@ class AcercadePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   GestureDetector(
-                    onTap: () => _showImageDialog(context, '/img/pluriverso.png'),
+                    onTap: () => _showImageDialog(context, '/img/logopluriversos.png'),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: Image.asset(
-                        '/img/pluriverso.png',
-                        width: 100,
-                        height: 100,
+                        '/img/logopluriversos.png',
+                        width: 180,
+                        height: 180,
                       ),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () => _showImageDialog(context, '/img/concursoanillos.png'),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Image.asset(
-                        '/img/concursoanillos.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () => _showImageDialog(context, '/img/ministerio.png'),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Image.asset(
-                        '/img/ministerio.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                    ),
-                  ),
+                 
                 ],
               ),
               SizedBox(
